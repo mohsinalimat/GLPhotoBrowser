0f37eeb0f1918510ebbcf37f91875bf67b5e9a91
高力 <3071730@qq.com>
2016-05-04 18:53:38 +0800

    添加 GLPhotoBrowserViewController

---
+++ GLPhotoBrowser/Classes/GLPhotoBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLPhotoBrowserViewController.h
+//  Pods
+//
+//  Created by 高力 on 16/5/4.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLPhotoBrowserViewController : UIViewController
+
+@end
