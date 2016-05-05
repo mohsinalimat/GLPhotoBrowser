17dd4827dcb93e3357960ff1d3ffaf276d19f904
高力 <3071730@qq.com>
2016-05-05 10:15:59 +0800

    添加 GLPhotoScrollView

---
+++ GLPhotoBrowser/Classes/View/GLPhotoScrollView.h
@@ -0,0 +1,13 @@
+//
+//  GLPhotoScrollView.h
+//  Pods
+//
+//  Created by 高力 on 16/5/5.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLPhotoScrollView : UIScrollView
+
+@end
---
+++ GLPhotoBrowser/Classes/View/GLPhotoScrollView.m
@@ -0,0 +1,21 @@
+//
+//  GLPhotoScrollView.m
+//  Pods
+//
+//  Created by 高力 on 16/5/5.
+//
+//
+
+#import "GLPhotoScrollView.h"
+
+@implementation GLPhotoScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
