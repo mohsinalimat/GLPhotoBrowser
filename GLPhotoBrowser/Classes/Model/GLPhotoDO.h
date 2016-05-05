e444dce5c7451e3908c010d445bd2a00b36be831
高力 <3071730@qq.com>
2016-05-05 14:57:08 +0800

    添加 GLPhotoDO

---
+++ GLPhotoBrowser/Classes/Model/GLPhotoDO.h
@@ -0,0 +1,13 @@
+//
+//  GLPhotoDO.h
+//  Pods
+//
+//  Created by 高力 on 16/5/5.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLPhotoDO : NSObject
+
+@end
---
+++ GLPhotoBrowser/Classes/Model/GLPhotoDO.m
@@ -0,0 +1,13 @@
+//
+//  GLPhotoDO.m
+//  Pods
+//
+//  Created by 高力 on 16/5/5.
+//
+//
+
+#import "GLPhotoDO.h"
+
+@implementation GLPhotoDO
+
+@end
