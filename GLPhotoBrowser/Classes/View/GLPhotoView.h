90049d4766e5bfe5b3cefd3035102e3d5cf81e12
高力 <3071730@qq.com>
2016-05-05 10:18:26 +0800

    添加 GLPhotoView

---
+++ GLPhotoBrowser/Classes/View/GLPhotoView.h
@@ -0,0 +1,13 @@
+//
+//  GLPhotoView.h
+//  Pods
+//
+//  Created by 高力 on 16/5/5.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLPhotoView : UIScrollView
+
+@end
---
+++ GLPhotoBrowser/Classes/View/GLPhotoView.m
@@ -0,0 +1,21 @@
+//
+//  GLPhotoView.m
+//  Pods
+//
+//  Created by 高力 on 16/5/5.
+//
+//
+
+#import "GLPhotoView.h"
+
+@implementation GLPhotoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
