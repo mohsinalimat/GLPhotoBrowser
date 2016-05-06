67c98c699a294b4b5e8f8ef952e6109a6514c434
高力 <3071730@qq.com>
2016-05-06 09:35:02 +0800

    添加 GLPhotoBrowserViewModel

---
+++ GLPhotoBrowser/Classes/ViewModel/GLPhotoBrowserViewModel.h
@@ -0,0 +1,13 @@
+//
+//  GLPhotoBrowserViewModel.h
+//  Pods
+//
+//  Created by 高力 on 16/5/6.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLPhotoBrowserViewModel : NSObject
+
+@end
---
+++ GLPhotoBrowser/Classes/ViewModel/GLPhotoBrowserViewModel.m
@@ -0,0 +1,13 @@
+//
+//  GLPhotoBrowserViewModel.m
+//  Pods
+//
+//  Created by 高力 on 16/5/6.
+//
+//
+
+#import "GLPhotoBrowserViewModel.h"
+
+@implementation GLPhotoBrowserViewModel
+
+@end
