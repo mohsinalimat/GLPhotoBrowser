a5f9c4bee94bec0638492e221c9ee0bf087dd70b
高力 <3071730@qq.com>
2016-05-09 15:25:08 +0800

    更新演示

--- Example/GLPhotoBrowser/GLViewController.m
+++ Example/GLPhotoBrowser/GLViewController.m
@@ -63,6 +63,8 @@
     GLCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"GLCollectionViewCell" forIndexPath:indexPath];
     
     if (cell) {
+        cell.tag = indexPath.row;
+        
         [cell.thumbnail sd_setImageWithURL:[NSURL URLWithString:self.middleUrlArray[indexPath.row]]];
     }
     
@@ -70,6 +72,37 @@
 }
 
 #pragma mark - UICollectionViewDelegate
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+
+    NSArray        *urlArray = @[@"http://image.tianjimedia.com/uploadImages/2015/343/21/EZ1YHO659YV8.jpg",
+                                 @"http://image.tianjimedia.com/uploadImages/2015/343/52/FB7GR8II5GS5.jpg",
+                                 @"http://image.tianjimedia.com/uploadImages/2015/343/18/52E10034H217.jpg",
+                                 @"http://image.tianjimedia.com/uploadImages/2015/343/57/7UZWDIG9M922.jpg",
+                                 @"http://image.tianjimedia.com/uploadImages/2015/343/13/O5A4WH50VBS8.jpg",
+                                 @"http://image.tianjimedia.com/uploadImages/2015/343/39/49O09SDO2RC2.jpg",
+                                 @"http://image.tianjimedia.com/uploadImages/2015/343/05/W1X2E3JY2N4G.jpg",
+                                 @"http://image.tianjimedia.com/uploadImages/2015/343/09/057LJ5B49V79.jpg",
+                                 @"http://image.tianjimedia.com/uploadImages/2015/343/48/952B2U4K05C7.jpg"];
+    
+    NSMutableArray *photoDOs = [NSMutableArray array];
+    
+    for (int i = 0; i < urlArray.count; i++) {
+        GLPhotoDO *photoDO = [[GLPhotoDO alloc] init];
+        
+        photoDO.url       = urlArray[i];
+        photoDO.thumbnail = [self.view viewWithTag:i];
+        
+        [photoDOs addObject:photoDO];
+    }
+    
+    GLPhotoBrowserViewModel      *photoBrowserViewModel      = [[GLPhotoBrowserViewModel alloc] initWithData:photoDOs index:indexPath.row];
+    GLPhotoBrowserViewController *photoBrowserViewController = [[GLPhotoBrowserViewController alloc] init];
+    
+    photoBrowserViewController.viewModel = photoBrowserViewModel;
+    
+    [self.navigationController pushViewController:photoBrowserViewController animated:NO];
+}
 
 #pragma mark - getters and setters
 
